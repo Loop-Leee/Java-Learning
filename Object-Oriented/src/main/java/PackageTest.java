a15e844c84d376a7620a798be2ec78af117d96e6
Alec-Lee777 <lilongpu123@foxmail.com>
2024-03-01 13:07:40 +0800

    学习:import导入包

---
+++ Object-Oriented/src/main/java/PackageTest.java
@@ -0,0 +1,17 @@
+/**
+ * ClassName: PackageTest
+ * Package: PACKAGE_NAME
+ * Description:
+ *
+ * @Author alec
+ * @Create 2024/3/1 12:44
+ * @Version 1.0
+ */
+
+// import animals.Animal;
+
+public class PackageTest {
+
+
+
+}
