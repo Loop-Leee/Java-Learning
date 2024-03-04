6e8469613509b77f82f1c1e6c70ff2a9b7724716
Alec-Lee777 <140378249+Alec-Lee777@users.noreply.github.com>
2024-03-04 14:10:13 +0800

    学习：文件写入

---
+++ FileInputOutput-learning/src/main/java/com/alec/learning/OutputTest.java
@@ -0,0 +1,19 @@
+package com.alec.learning;
+
+import java.io.*;
+
+public class OutputTest {
+    public static void main(String[] args)  {
+        try {
+            // BufferedWriter 本身不提供处理字符流的功能，它只用来缓存字符流
+            // OutputStreamWriter 用于处理字节流，将其转换为字符流
+            // OutputStream 是字符流本身
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("./FileInputOutput-learning/test.txt")));
+            out.write("Hello World!");
+            out.close();
+            System.out.println("文件创建成功!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
