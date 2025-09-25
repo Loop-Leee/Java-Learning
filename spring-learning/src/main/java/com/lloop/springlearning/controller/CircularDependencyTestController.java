184e0ca76ab164cdb6c2e11e4b0068a290f798f6
lloop <alecleeeeee@gmail.com>
2025-09-25 09:39:25 +0800

    fix: 提交一些忘记提交的文件

---
+++ spring-learning/src/main/java/com/lloop/springlearning/controller/CircularDependencyTestController.java
@@ -0,0 +1,25 @@
+package com.lloop.springlearning.controller;
+
+import com.lloop.springlearning.service.AService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author lloop
+ * @Create 2025/9/25 09:30
+ * 测试循环依赖注入是否成功
+ */
+@RestController
+public class CircularDependencyTestController {
+
+    @Autowired
+    private AService aService;
+
+    @GetMapping("/test-circular-dependency")
+    public String testCircularDependency() {
+        System.out.println("开始测试循环依赖...");
+        aService.doSomethingWithB();
+        return "循环依赖测试完成！请查看控制台输出。";
+    }
+}
