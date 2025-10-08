48851e46ef797a219738aebb8895a68fcf3ec681
lloop <alecleeeeee@gmail.com>
2025-10-08 12:37:28 +0800

    设计模式: 单例模式(补充注释)

--- design-pattern-learning/src/main/java/com/lloop/designpatternlearning/singleton/Singleton.java
+++ design-pattern-learning/src/main/java/com/lloop/designpatternlearning/singleton/Singleton.java
@@ -6,7 +6,7 @@ package com.lloop.designpatternlearning.singleton;
  * @Description
  * 单例模式
  * 通过synchronize确保只有一个线程进入同步块创建实例
- * 通过volatile确保对象引用的可见性以及创建过程的有序性,避免发生指令重排序
+ * 通过volatile确保对象引用的可见性以及创建过程的有序性,避免发生指令重排序, 否则其他线程有可能提前拿到未进行初始化的引用
  */
 public class Singleton {
 
@@ -16,6 +16,12 @@ public class Singleton {
         if(instance == null) {
             synchronized (Singleton.class) {
                 if(instance == null) {
+                    /**
+                     * 创建实例的三个步骤(指令重排可能发生, 需要添加volatile修饰符进行避免)
+                     * 1. 分配内存
+                     * 2. 初始化(实例化)对象
+                     * 3. 设置instance的引用
+                     */
                     instance = new Singleton();
                 }
             }
