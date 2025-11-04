7899c329cb0732becd54ec80536fda21488d4b37
lloop <alecleeeeee@gmail.com>
2025-11-04 13:09:53 +0800

    fix: 补充提交测试类

---
+++ concurrent-learning/src/test/java/com/lloop/concurrentlearning/ConcurrentLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lloop.concurrentlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrentLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
